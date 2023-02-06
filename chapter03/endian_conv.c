78c22184b40acb7ced1df95be434e4822b1e7c73
Zeyuan Zhao <48199429+zzyzeyuan@users.noreply.github.com>
2023-02-06 11:59:01 +0800

    Update endian_conv.c

--- chapter03/endian_conv.c
+++ chapter03/endian_conv.c
@@ -3,6 +3,8 @@
 
 int main(int argc, char *argv[])
 {
+	// 大端序：高字节放低地址  比如0x1234   12就是高字节34就是低字节  如果按大端序存放则存为12 34
+	// 小端序：低字节放低地址  比如0x1234   34是低字节  按小端序存则为 34 12
 	unsigned short host_port=0x1234;
 	unsigned short net_port;
 	unsigned long host_addr=0x12345678;
