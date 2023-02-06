133911fad0b75cdb0310afe46bd1d9d78f2df5af
Zeyuan Zhao <48199429+zzyzeyuan@users.noreply.github.com>
2023-02-06 11:55:13 +0800

    字节序的转换   小端序  大端序（网络字节序也是大端序）

--- chapter03/endian_conv.c
+++ chapter03/endian_conv.c
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
 	unsigned short net_port;
 	unsigned long host_addr=0x12345678;
 	unsigned long net_addr;
-	
+	//htons 中h表示主机(host)字节序  n表示网络字节序   s表示short  htonl中l表示long
+	// htons可以解释为“将short型数据从主机字节序转为网络字节序”
 	net_port=htons(host_port);
     // host_port = 0x1234 小端序
     // net_port = 0x3412
